d2ecd72063c61342b3d90cde68b57615257c446c
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-04-20 17:29:50 +0100

    Fixed the command line challenges

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,12 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import sun.misc.Unsafe
 import java.io.File
 import java.lang.reflect.Field
-import java.lang.reflect.Method
-import sun.misc.Unsafe
 
 @Composable
 @Preview
@@ -36,7 +31,7 @@ fun App() {
                        backendNote = "An error occurred: ${ex.message}"
                        println("Error Starting Backend serve")
                    }
-               } else if(process != null){
+               } else {
                    try {
                        Runtime.getRuntime().exec("kill ${getUnixPid(process!!)}")
                        backendNote = "Restart Server"
